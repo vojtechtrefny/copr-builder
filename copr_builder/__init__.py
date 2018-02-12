310e4ce204747a21964ea8ecbc369baa7eaf78e6
Jiri Konecny <jkonecny@redhat.com>
2018-02-12 14:23:54 +0100

    Add support for pre archive command
    New configuration value (pre_archive_cmd) is supported.

--- copr_builder/__init__.py
+++ copr_builder/__init__.py
@@ -6,6 +6,7 @@ COPR_REPO_CONF = 'copr_repo'
 GIT_URL_CONF = 'git_url'
 GIT_BRANCH_CONF = 'git_branch'
 GIT_MERGE_BRANCH_CONF = 'git_merge_branch'
+PRE_ARCHIVE_CMD_CONF = 'pre_archive_cmd'
 ARCHIVE_CMD_CONF = 'archive_cmd'
 
 
