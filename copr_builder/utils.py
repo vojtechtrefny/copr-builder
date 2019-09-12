6eeca46a5cdf6abb1329852a4abc691763408616
Vojtech Trefny <vtrefny@redhat.com>
2019-09-12 14:08:14 +0200

    Run all commands with C locale
    Fixes: #4

--- copr_builder/utils.py
+++ copr_builder/utils.py
@@ -1,9 +1,13 @@
+import os
 import subprocess
 
 
 def run_command(command, cwd=None):
+    env = os.environ.copy()
+    env["LC_ALL"] = "C"
+
     res = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE,
-                           stderr=subprocess.PIPE, cwd=cwd)
+                           stderr=subprocess.PIPE, cwd=cwd, env=env)
 
     out, err = res.communicate()
     if res.returncode != 0:
