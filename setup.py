64cdfe4be1b907a82df82dec890a2546b321affd
Vojtech Trefny <vtrefny@redhat.com>
2022-04-25 09:21:41 +0200

    Use setuptools instead of distutils in setup.py
    The distutils module is deprecated and will be removed in Python
    3.12.

--- setup.py
+++ setup.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
 
-import setuptools  # pylint: disable=unused-import
-
-from distutils.core import setup
+import setuptools
 
 
 with open("README.rst", "r", encoding="utf-8") as f:
     long_description = f.read()
 
 
-setup(name='copr-builder',
-      version='1.0',
-      description='Python script for building git projects in Copr',
-      long_description=long_description,
-      long_description_content_type="text/x-rst",
-      author='Vojtech Trefny',
-      author_email='vtrefny@redhat.com',
-      url='https://github.com/vojtechtrefny/copr-builder',
-      packages=['copr_builder'],
-      scripts=['copr-builder'])
+setuptools.setup(name='copr-builder',
+                 version='1.0',
+                 description='Python script for building git projects in Copr',
+                 long_description=long_description,
+                 long_description_content_type="text/x-rst",
+                 author='Vojtech Trefny',
+                 author_email='vtrefny@redhat.com',
+                 url='https://github.com/vojtechtrefny/copr-builder',
+                 packages=['copr_builder'],
+                 scripts=['copr-builder'])
